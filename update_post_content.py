e4c4e3960d12379229e8c059a3ffe311740df259
Marco <marco@romagnolo.me>
2025-03-11 11:38:10 +0100

    Add update source link

--- update_post_content.py
+++ update_post_content.py
@@ -149,7 +149,7 @@ def update_img_src(post_id, post_content):
 def update_custom_tag(post_id, post_content):
     # Regular expression pattern to find all caption shortcodes
     pattern = re.compile(
-        r'<p>Fonte:.*?<a href="([^"]+)">https?://[^<]+</a>.*?</p>',
+        r'<p>Fonti:.*?<a href="([^"]+)">https?://[^<]+</a>.*?</p>',
         re.DOTALL | re.IGNORECASE
     )
 
