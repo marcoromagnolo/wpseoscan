d240128ecb92b518c29d62f759e63818cc5776b4
Marco <marco@romagnolo.me>
2025-03-17 00:05:06 +0100

    Update settings and enhance WordPress query parameters

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Settings
 
 WEB_SETTINGS = {
-    'host': '127.0.0.1',
+    'host': 'localhost',
     'port': '5050',
     'debug': True,
     'use_reloader': False
@@ -21,18 +21,21 @@ DB_SETTINGS = {
 }
 
 WORK_DIR = 'storage'
-OPENAI_SECRET = ''
+OPENAI_SECRET = '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'
 
 WP_QUERY = {
-    'select_posts_from_date': '2014-01-01 00:00:00',
-    'select_posts_to_date': '2026-12-06 00:00:00'
+    'select_posts_from_date': '2021-01-23 00:00:00',
+    'select_posts_to_date': '2021-12-31 00:00:00',
+    'post_author': 2
 }
 
-BASE_URL = ''
+BLACKLIST_ENTITIES = ['iflscience', 'sciencealert', 'interestingengineering']
+
+BASE_URL = 'https://www.scienzenotizie.it'
 
 WP_SETTINGS = {
     'host': 'localhost',
-    'user': 'user',
+    'user': 'scienzenotizie',
     'password': 'password',
-    'database': 'db'
+    'database': 'scienzenotizie'
 }
